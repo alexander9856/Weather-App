2ca5321665e809a343246c498b1f39bc7d9c1336
Alexander Gochev <124374696+alexander9856@users.noreply.github.com>
2023-06-04 16:26:05 +0300

    update search

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fc1bbbdaa8mshb9690e7d86705a3p10937bjsnfa06c2edef9c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
--- src/components/Header/Search.js
+++ src/components/Header/Search.js
@@ -1,7 +1,30 @@
 import { useState } from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate';
+import { GEO_API_URL, geoApiOptions } from '../../api'
 export const Search = () => {
-    const [search, setSearch] = useState('');
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = async (inputValue) => {
+        let data = [];
+        try {
+            const response = await fetch(`${GEO_API_URL}/cities?minPopulation=5000&namePrefix=${inputValue}`, geoApiOptions)
+            const res = await response.json();
+            let filteredData = res.data.filter((obj, index) => res.data.findIndex((item) => item.name === obj.name) === index);
+            data = {
+                options: filteredData.map(x => {
+                    return {
+                        value: `${x.latitude} ${x.longitude}`,
+                        label: `${x.name}, ${x.countryCode}`
+                    }
+                })
+            }
+            return data
+        }
+        catch (err) {
+            console.log(err.message)
+        }
+
+    }
     const handleOnChange = (searchValue) => {
         setSearch(searchValue)
     }
@@ -11,6 +34,7 @@ export const Search = () => {
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
