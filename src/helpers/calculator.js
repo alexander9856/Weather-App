52aa303539e1e6450241430a806e8124e36e5042
Alexander Gochev <124374696+alexander9856@users.noreply.github.com>
2023-07-16 12:26:17 +0300

    update calculator

--- src/helpers/calculator.js
+++ src/helpers/calculator.js
@@ -1,18 +1,45 @@
-export const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"];
+export const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const average = (sum, itemsCount) => {
-    return Math.round(sum / itemsCount)
-}
+const average = (sum, itemsCount) => Math.round(sum / itemsCount);
+
 export const calculator = (data) => {
     const dayLight = data?.filter(x => x.weather[0].icon.includes('d'));
-    const sumMaxDegrees = dayLight.reduce((a, b) => a + b.main.temp_max, 0);
-    const sumMinDegrees = dayLight.reduce((a, b) => a + b.main.temp_min, 0);
-    const sumPressure = dayLight.reduce((a, b) => a + b.main.pressure, 0);
-    const sumHumidity = dayLight.reduce((a, b) => a + b.main.humidity, 0);
-    const sumClouds = dayLight.reduce((a, b) => a + b.clouds.all, 0);
-    const sumWindSpeed = dayLight.reduce((a, b) => a + b.wind.speed, 0);
-    const sumSeaLevel = dayLight.reduce((a, b) => a + b.main.sea_level, 0);
-    const sumFeelsLike = dayLight.reduce((a, b) => a + b.main.feels_like, 0);
+    const reducer = (acc, curr) => {
+        acc.sumMaxDegrees += curr.main.temp_max;
+        acc.sumMinDegrees += curr.main.temp_min;
+        acc.sumPressure += curr.main.pressure;
+        acc.sumHumidity += curr.main.humidity;
+        acc.sumClouds += curr.clouds.all;
+        acc.sumWindSpeed += curr.wind.speed;
+        acc.sumSeaLevel += curr.main.sea_level;
+        acc.sumFeelsLike += curr.main.feels_like;
+        acc.icons.push(curr.weather[0].icon);
+        return acc;
+    };
+
+    const initialValue = {
+        sumMaxDegrees: 0,
+        sumMinDegrees: 0,
+        sumPressure: 0,
+        sumHumidity: 0,
+        sumClouds: 0,
+        sumWindSpeed: 0,
+        sumSeaLevel: 0,
+        sumFeelsLike: 0,
+        icons: []
+    };
+
+    const {
+        sumMaxDegrees,
+        sumMinDegrees,
+        sumPressure,
+        sumHumidity,
+        sumClouds,
+        sumWindSpeed,
+        sumSeaLevel,
+        sumFeelsLike,
+        icons
+    } = dayLight.reduce(reducer, initialValue);
 
     const averageMinDegrees = Math.floor(sumMinDegrees / dayLight.length);
     const averageMaxDegrees = Math.ceil(sumMaxDegrees / dayLight.length);
@@ -23,24 +50,18 @@ export const calculator = (data) => {
     const averageSeaLevel = average(sumSeaLevel, dayLight.length);
     const averageFeelsLike = average(sumFeelsLike, dayLight.length);
 
+    const iconsCount = icons.reduce((acc, icon) => {
+        acc[icon] = (acc[icon] || 0) + 1;
+        return acc;
+    }, {});
+
+    const maxOccurrences = Math.max(...Object.values(iconsCount));
+    const icon = Object.keys(iconsCount).find(key => iconsCount[key] === maxOccurrences);
+
     const middleIndex = Math.floor(dayLight.length / 2);
-
-    const icons = [];
-    dayLight.map(x => icons.push(x.weather[0].icon));
-
-    const occurancesCounts = {};
-    icons.map(x => occurancesCounts[x] = (occurancesCounts[x] || 0) + 1);
-
-    let arr = Object.values(occurancesCounts);
-    let max = Math.max(...arr);
-
-    let icon = ''
-    for (let i in occurancesCounts) {
-        if (max == occurancesCounts[i]) icon = i
-    }
-    
-    const date = dayLight[middleIndex].dt_txt
+    const date = dayLight[middleIndex].dt_txt;
     const day = weekDays[new Date(date).getDay()];
+
     return {
         averageMaxDegrees,
         averageMinDegrees,
@@ -52,5 +73,5 @@ export const calculator = (data) => {
         averageFeelsLike,
         icon,
         day
-    }
-}
+    };
+};
